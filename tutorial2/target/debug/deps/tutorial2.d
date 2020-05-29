4a826697cfdd4135d9a35394dcaaea0ecb7c5ebb
ronidas39 <49780975+ronidas39@users.noreply.github.com>
2020-05-29 09:10:00 +0200

    turorials

---
+++ tutorial2/target/debug/deps/tutorial2.d
@@ -0,0 +1,5 @@
+C:\RUST_TUTORIAL\RUST_TUTORIAL\tutorial2\target\debug\deps\tutorial2.exe: src\main.rs
+
+C:\RUST_TUTORIAL\RUST_TUTORIAL\tutorial2\target\debug\deps\tutorial2.d: src\main.rs
+
+src\main.rs:
---
+++ tutorial2/target/debug/tutorial2.d
@@ -0,0 +1,1 @@
+C:\RUST_TUTORIAL\RUST_TUTORIAL\tutorial2\target\debug\tutorial2.exe: C:\RUST_TUTORIAL\RUST_TUTORIAL\tutorial2\src\main.rs
